87f7fee4ce4f2fe742d2e6ac2bad10de1f3cc4d7
BJocelyn <jocelyn.bartolo@outlook.com>
2026-07-14 20:40:57 -0600

    feat: About me exercise

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,37 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Jocelyn Bartolo';
+  const age = 27;
+  const hobbies = [
+    'Reading',
+    'Music',
+    'Swimming and Exercising',
+    'Cooking',
+    'Painting',
+  ];
+
   return (
     <div>
-      {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+
+      <p>
+        {' '}
+        My name is {name} and I am {age} years old.{' '}
+      </p>
+      <p>
+        {' '}
+        I am learning how to program using React. Although it is a long and
+        challenging journey, I am enjoying the process and I hope to continue
+        learning and improving my skills.{' '}
+      </p>
+
+      <h2>My Hobbies</h2>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
